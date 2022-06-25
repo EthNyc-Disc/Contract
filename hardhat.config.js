e3238765f1355b51d9599cd32fb567b614e993ae
Jaival Patel <pateljaival05@gmail.com>
2022-06-24 23:27:53 -0400

    hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
+
+const {API_URL, PRIVATE_KEY} = process.env;
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +22,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    mumbai: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    goreli: {
+      url: 'https://eth-goerli.alchemyapi.io/v2/xQ2sUXLPomZOTiFZpe82afJwzSRDB5Od',
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
